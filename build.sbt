eb9eda88b53eb9eb216975c7acaca2faebe94ea7
Raunak Jodhawat <raunakjodhawat@gmail.com>
2023-04-12 12:51:09 +0530

    first commit

---
+++ build.sbt
@@ -0,0 +1,26 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.10"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "akka-http"
+  )
+
+scalaVersion := "2.12.8"
+
+val akkaVersion = "2.8.0"
+val akkaHttpVersion = "10.5.0"
+val scalaTestVersion = "3.2.15"
+
+libraryDependencies ++= Seq(
+  // akka streams
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  // akka http
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion,
+  // testing
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+  "org.scalatest" %% "scalatest" % scalaTestVersion
+)
---
+++ src/main/scala/playground/Playground.scala
@@ -0,0 +1,39 @@
+package playground
+
+package playground
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import scala.io.StdIn
+
+object Playground extends App {
+
+  implicit val system = ActorSystem("AkkaHttpPlayground")
+  import system.dispatcher
+
+  val simpleRoute =
+    pathEndOrSingleSlash {
+      complete(
+        HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+          |<html>
+          | <body>
+          |   Rock the JVM with Akka HTTP!
+          | </body>
+          |</html>
+        """.stripMargin
+        )
+      )
+    }
+
+  val bindingFuture =
+    Http().newServerAt("localhost", 8080).bindFlow(simpleRoute)
+  // wait for a new line, then terminate the server
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
+}
